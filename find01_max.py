43e44ea92ba9212bd37456d47ec604f19860ed11
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-03-29 08:54:42 +0500

    find01_max.py

--- find01_max.py
+++ find01_max.py
@@ -5,5 +5,12 @@ def find_max(data):
         data: list of numbers
     returns: maximum number in the list
     """
-    return 0
-    
+    i = 0
+    n = 0
+    while i < len(data):
+        if data[i] > n:
+            n = data[i]
+        i += 1
+    return n
+
+print(find_max([1, 2, 3, 4, 5]))
