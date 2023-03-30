7da65d05ca56c9f437cb74eab8d93943eca735c4
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-03-30 07:16:08 +0500

    find06

--- find06_max_odd.py
+++ find06_max_odd.py
@@ -5,4 +5,14 @@ def find_max_odd(data):
         data: list of numbers
     returns: maximum odd number in the list
     """
-    return 0
+    i = 0
+    n = 0
+
+    while i < len(data):
+        if data[i]%2 == 1 and data[i] > n:
+            n = data[i]
+        i += 1
+    return n
+    # return -1
+
+print(find_max_odd([11, 7, 51, 4, 91]))
