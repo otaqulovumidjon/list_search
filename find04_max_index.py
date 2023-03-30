f3a1648d88b625e0d9ff2d4e7a3a73db9bf7856c
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-03-30 07:14:34 +0500

    find04

--- find04_max_index.py
+++ find04_max_index.py
@@ -5,4 +5,15 @@ def find_max_index(data):
         data: list of numbers
     returns: index of maximum number in the list
     """
-    return 0
+    i = 0
+    n = 0
+    s = 0
+
+    while i < len(data):
+        if data[i] > n:
+            n = data[i]
+            s = i
+        i += 1
+    return s
+
+print(find_max_index([6, 8, 7, 4, 0]))
